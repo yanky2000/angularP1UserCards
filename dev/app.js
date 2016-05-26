5d0c2c2b07c67eb42b38ac47ff034019c6db3fe4
VY <yanky2000@yahoo.com>
2016-05-26 16:55:29 +0300

    refactor with services

--- dev/app.js
+++ dev/app.js
@@ -1,76 +1,62 @@
 var app = angular.module('myApp', []);
 
+app.service('usersData', function ($http) {
+  this.getUsers = function () {
+    return $http.get('http://jsonplaceholder.typicode.com/users/');
+  }
+})
+
+  .service('uploadUsers', function () {
+    return
+  })
+
 app.component('avatar', {
   bindings: {
     user: '='
   },
   restrict: 'E',
   templateUrl: 'avatar.html'
-});
+})
 
-app.component('usercard', {
-  bindings: {
-    user: '=',
-  },
-  restrict: 'E',
-  templateUrl: 'user.component.html'
-});
+  .component('usercard', {
+    bindings: {
+      user: '=',
+    },
+    restrict: 'E',
+    templateUrl: 'user.component.html'
+  })
 
-app.component('usercards', {
-  templateUrl: 'users.component.html',
-  controller: function () {
-    this.users = [{
-      fullName: 'Иванов Иван',
-      avatarUrl: 'https://randomuser.me/api/portraits/thumb/men/57.jpg',
-      birthdate: '1976-10-10',
-      gender: 'мужской',
-      address: 'ул. Звенигороская, 47б',
-      email: 'ivanov@mail.ru'
-    }, {
-        fullName: 'Петров Петр',
-        avatarUrl: 'https://randomuser.me/api/portraits/thumb/men/7.jpg',
-        birthdate: '1957-01-14',
-        gender: 'мужской',
-        address: 'ул.Пушкиская, 13',
-        email: 'ivanov@mail.ru'
-      }, {
-        fullName: 'Натальина Наталья',
-        avatarUrl: 'https://randomuser.me/api/portraits/thumb/women/7.jpg',
-        birthdate: '1990-07-03',
-        gender: 'женский',
-        address: 'ул. Лермонтова, 59',
-        email: 'ivanov@mail.ru'
-      }];
-  }
-});
+  .component('usercards', {
+    templateUrl: 'users.component.html',
+    controller: function (usersData) {
+      var self = this;
 
-app.directive('usercards2', function () {
-  return {
-    link: function (scope) {
-      scope.users = [{
-        fullName: 'Иванов Иван',
-        avatarUrl: 'https://randomuser.me/api/portraits/thumb/men/57.jpg',
-        birthdate: '1976-10-10',
-        gender: 'мужской',
-        address: 'ул. Звенигороская, 47б',
-        email: 'ivanov@mail.ru'
-      }, {
-          fullName: 'Петров Петр',
-          avatarUrl: 'https://randomuser.me/api/portraits/thumb/men/7.jpg',
-          birthdate: '1957-01-14',
-          gender: 'мужской',
-          address: 'ул.Пушкиская, 13',
-          email: 'ivanov@mail.ru'
-        }, {
-          fullName: 'Натальина Наталья',
-          avatarUrl: 'https://randomuser.me/api/portraits/thumb/women/7.jpg',
-          birthdate: '1990-07-03',
-          gender: 'женский',
-          address: 'ул. Лермонтова, 59',
-          email: 'ivanov@mail.ru'
-        }];
+      usersData.getUsers().then(function (res) {
+        console.log(res.data[0]);
+        return self.users = res.data;
+      });
+
+      // this.users = [{
+      //   fullName: 'Иванов Иван',
+      //   avatarUrl: 'https://randomuser.me/api/portraits/thumb/men/57.jpg',
+      //   birthdate: '1976-10-10',
+      //   gender: 'мужской',
+      //   address: 'ул. Звенигороская, 47б',
+      //   email: 'ivanov@mail.ru'
+      // }, {
+      //     fullName: 'Петров Петр',
+      //     avatarUrl: 'https://randomuser.me/api/portraits/thumb/men/7.jpg',
+      //     birthdate: '1957-01-14',
+      //     gender: 'мужской',
+      //     address: 'ул.Пушкиская, 13',
+      //     email: 'ivanov@mail.ru'
+      //   }, {
+      //     fullName: 'Натальина Наталья',
+      //     avatarUrl: 'https://randomuser.me/api/portraits/thumb/women/7.jpg',
+      //     birthdate: '1990-07-03',
+      //     gender: 'женский',
+      //     address: 'ул. Лермонтова, 59',
+      //     email: 'ivanov@mail.ru'
+      //   }];
     }
-  }
-});
-
-        //console.log(this.users[1].fullName)
+  });
---
+++ dist/post.js
@@ -0,0 +1,38 @@
+$(function () {
+    
+var users = [
+    {
+        fullName: 'Иванов Иван',
+        avatarUrl: 'https://randomuser.me/api/portraits/thumb/men/57.jpg',
+        birthdate: '1976-10-10',
+        gender: 'мужской',
+        address: 'ул. Звенигороская, 47б',
+        email: 'ivanov@mail.ru'
+    }, {
+        fullName: 'Петров Петр',
+        avatarUrl: 'https://randomuser.me/api/portraits/thumb/men/7.jpg',
+        birthdate: '1957-01-14',
+        gender: 'мужской',
+        address: 'ул.Пушкиская, 13',
+        email: 'ivanov@mail.ru'
+    }, {
+        fullName: 'Натальина Наталья',
+        avatarUrl: 'https://randomuser.me/api/portraits/thumb/women/7.jpg',
+        birthdate: '1990-07-03',
+        gender: 'женский',
+        address: 'ул. Лермонтова, 59',
+        email: 'ivanov@mail.ru'
+    }];
+
+var root = 'http://jsonplaceholder.typicode.com';
+// var root = ' http://test-api.javascript.ru/vyang/users';
+
+// $.ajax({
+//   url: root + '/users/',
+//   method: 'GET'
+// }).then(function(data) {
+//   console.log(data);
+// });
+
+
+});
